21edac2fef380a011a43e1045e2a15efaf4572d6
harsh-dynasty <harshsoni0310@gmail.com>
2020-12-01 21:50:35 +0530

    initial commit

--- public/js/register.js
+++ public/js/register.js
@@ -1,5 +1,6 @@
 
 const localStorage=window.localStorage;
+
 function isLoggedIn(){
     
     if(localStorage.getItem('username')!=undefined)
@@ -8,24 +9,23 @@ function isLoggedIn(){
 isLoggedIn();
 document.querySelector('form').addEventListener('submit',(e)=>{
     e.preventDefault();
-    var username=document.getElementById('username').value;
-    var password=document.getElementById('password').value;
-    
-    fetch("/login",{
+    const name=document.getElementById('name').value;
+    const username=document.getElementById('username').value;
+    const password=document.getElementById('password').value;
+    fetch("/register",{
         method:'POST',
-        body: JSON.stringify({username,password}),
+        body: JSON.stringify({name,username,password}),
         headers: {
             'Content-Type': 'application/json'
         }
 
     }).then(res=>res.json())
     .then(data=>{
-        if(data.mssg=="Success"){
-            localStorage.setItem('username',username);
-            window.open('/voucher','_self');
-            
+        if(data.mssg=='Successfully registered'){
+            alert(data.mssg);
+            window.open('/','_self');
         }
-        else{
+        else {
             document.getElementById('message').textContent=data.mssg;
             setTimeout(()=>{document.getElementById('message').textContent="";},1500);
         }
--- server.js
+++ server.js
@@ -1,135 +1,115 @@
 const express=require('express');
 const app=express();
 const port = process.env.PORT || 3000;
-app.listen(port);
+app.listen(port,()=>console.log(`Server started at port ${port}...`));
 
-var users={};
 
+///////////////////////
 const mongoose = require('mongoose');
  
 mongoose.connect('mongodb+srv://dbUser:1234@cluster0.afpe2.mongodb.net/eaccounting?retryWrites=true&w=majority')
     .then(() => 'You are now connected to Mongo!')
     .catch(err => console.error('Something went wrong', err));
 
-const userSchema = mongoose.Schema({
-    ip:String,
+const registeruserSchema = mongoose.Schema({
+    name:String,
     username:String,
+    password:String
 });
 
-var Users=mongoose.model('active_users',userSchema);
-
-
+var regUsers=mongoose.model('registered_users',registeruserSchema);
+///////////////////////
 app.use(express.static('public'));
-app.use(express.urlencoded({extended:false}));
+app.use(express.json());
 
-app.get('/user',(req,res)=>{
-    eval(`res.json({username:users["${req.ip}"]})`);
-   
-})
-
-app.get('/isLoggedIn',(req,res)=>{
-    if(eval(`users["${req.ip}"]==undefined`))
-        res.json({isLoggedIn:false});
-    else
-        res.json({isLoggedIn:true});
-    
-})
 app.get('/voucher',(req,res)=>{
-    if(eval(`users["${req.ip}"]==undefined`))
-        res.redirect('/');
-    else
-        res.sendFile('voucher.html',{root:"./public"});
-
-    
+   
+    res.sendFile('voucher.html',{root:"./public"});
 })
 app.get('/register',(req,res)=>{
-    if(eval(`users["${req.ip}"]==undefined`))
-        res.sendFile('register.html',{root:"./public"});
-    else
-        res.sendFile('voucher.html',{root:"./public"});
+    res.sendFile('register.html',{root:"./public"});
    
 })
 
 app.post('/login',(req,res)=>{
-    var username=req.body.name;
-    var password=req.body.password;
-    
-    if(username.length>0){  //validate username and password inside this block
-        
-        let user = {ip:req.ip,username:username};
-        Users.collection.insertOne(user)
-        .then(data=>console.log(data))
-        .catch(err=>console.log(err))
-        res.redirect("/voucher");
-        
-    }
-    
-})
-app.post('/register',(req,res)=>{
     var username=req.body.username;
     var password=req.body.password;
+    console.log({username,password});
+    //check whether username exists or not in regUsers
+    //if exists check for password
+    //if password matched add user to active user and open voucher
+    //if not matched give error mssg
+    //if username not exists give error username not registered
+    regUsers.find({username})
+    .then((docs)=>{
+        if(docs.length==0){
+            res.json({mssg:'Username not registered'});
+           
+        }
+        else{
+            if(docs[0].password==password){
+                res.json({mssg:'Success'});
+            }
+                
+            else
+                res.json({mssg:'Invalid password'});
+        }
+    })
+    .catch((err)=>{
+    console.log(err);
+    })
+
+})
+app.post('/register',(req,res)=>{
     var name=req.body.name;
-    if(username.length>0){  //save username and password in db after validating
-        
-        res.redirect("/voucher");
-        
-    }
+    var username=req.body.username;
+    var password=req.body.password;
+    //check whether username exists or not
+    //if exist err
+    //else save to database
+    regUsers.find({username}) 
+    .then((doc) => {
+       if (doc.length!=0) {
+         res.json({mssg:'User already registered'});
+       } else {
+        let Newuser = new regUsers({name,username,password});
+        regUsers.collection.insertOne(Newuser)
+          .then((data)=>{
+            res.json({mssg:'Successfully registered'});
+          }).catch((err)=>{
+            console.log(err);
+         })
+       }
+    })
+   .catch((err) => {
+     console.log(err);
+    });
 })
 
 
 app.get('/addToStocks',(req,res)=>{
-    if(eval(`users["${req.ip}"]==undefined`))
-        res.redirect('/');
-    else
-        res.sendFile('addToStocks.html',{root:"./public"});
+    res.sendFile('addToStocks.html',{root:"./public"});
 })
 app.get('/alterStocks',(req,res)=>{
-    if(eval(`users["${req.ip}"]==undefined`))
-        res.redirect('/');
-    else
-        res.sendFile('alterStocks.html',{root:"./public"});
+    res.sendFile('alterStocks.html',{root:"./public"});
 })
 app.get('/allStocks',(req,res)=>{
-    if(eval(`users["${req.ip}"]==undefined`))
-        res.redirect('/');
-    else
-        res.sendFile('allStocks.html',{root:"./public"});
+    res.sendFile('allStocks.html',{root:"./public"});
 })
 app.get('/purchase-sales',(req,res)=>{
-    if(eval(`users["${req.ip}"]==undefined`))
-        res.redirect('/');
-    else
-        res.sendFile('purchase-sales.html',{root:"./public"});
+    res.sendFile('purchase-sales.html',{root:"./public"});
 })
 app.get('/addAccount',(req,res)=>{
-    if(eval(`users["${req.ip}"]==undefined`))
-        res.redirect('/');
-    else
-        res.sendFile('adAccount.html',{root:"./public"});
+    res.sendFile('addAccount.html',{root:"./public"});
 })
 app.get('/alterAccount',(req,res)=>{
+    res.sendFile('alterAccount.html',{root:"./public"});
     
-    if(eval(`users["${req.ip}"]==undefined`))
-        res.redirect('/');
-    else
-        res.sendFile('alterAccount.html',{root:"./public"});
 })
 app.get('/allAccounts',(req,res)=>{
+    res.sendFile('allAccounts.html',{root:"./public"});
     
-    if(eval(`users["${req.ip}"]==undefined`))
-        res.redirect('/');
-    else
-        res.sendFile('allAccounts.html',{root:"./public"});
 })
 app.get('/contact',(req,res)=>{
-    if(eval(`users["${req.ip}"]==undefined`))
-        res.redirect('/');
-    else
-        res.sendFile('contact.html',{root:"./public"});
-})
-
-app.get('/signout',(req,res)=>{
-    console.log(eval(`users["${req.ip}"]`));
-    eval(`delete users["${req.ip}"]`);
-    res.redirect("/");
+    res.sendFile('contact.html',{root:"./public"});
 })
